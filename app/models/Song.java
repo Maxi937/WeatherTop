9d05b90f6b0250589ac8c70cce2477293b0060e3
Maxi937 <70072337+Maxi937@users.noreply.github.com>
2022-05-11 21:10:01 +0100

    - init WeatherTop

---
+++ app/models/Song.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Song {
+    // fields
+    public String title;
+    public String artist;
+    public double duration;
+
+    // constructor
+    public Song(String title, String artist, double duration){
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+    }
+}
