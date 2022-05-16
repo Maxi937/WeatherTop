70890a83116758bbac2fa46e32276e194e5ed906
Maxi937 <70072337+Maxi937@users.noreply.github.com>
2022-05-16 23:24:24 +0100

    - Version 2 Baseline - Columns style and variable readings

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -10,12 +10,14 @@ public class Reading extends Model
     public int code;
     public double temperature;
     public double windSpeed;
+    public int windPressure;
 
-    public Reading(int code, double temperature, double windSpeed)
+    public Reading(int code, double temperature, double windSpeed, int windPressure)
     {
         this.code = code;
         this.temperature = temperature;
         this.windSpeed = windSpeed;
+        this.windPressure = windPressure;
     }
 }
 
