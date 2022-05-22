ddde90df16666819dc61eea2f28f65515d10d7a0
Maxi937 <70072337+Maxi937@users.noreply.github.com>
2022-05-22 21:54:04 +0100

    - Release 1: add ability to add reading / move stations to own view

---
+++ app/controllers/StationCtrl.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.util.List;
+
+import models.Reading;
+import models.Station;
+import play.Logger;
+import play.mvc.Controller;
+
+public class StationCtrl extends Controller
+{
+    public static void index(Long id)
+    {
+        Station station = Station.findById(id);
+        Logger.info ("Station id = " + id);
+        render("station.html",station);
+    }
+
+    public static void addReading(Long id, int code, double temperature, double windSpeed, int windPressure, double windDirection)
+    {
+        Reading reading = new Reading(code, temperature, windSpeed, windPressure, windDirection);
+        Station station = Station.findById(id);
+        station.readings.add(reading);
+        station.save();
+        redirect ("/station/" + id);
+    }
+}
