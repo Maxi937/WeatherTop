607f0222841c696ed246419737213f5060a39779
Maxi937 <70072337+Maxi937@users.noreply.github.com>
2022-05-22 23:23:18 +0100

    - Release 2: Catch new Station latest reading error

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -18,4 +18,12 @@ public class Dashboard extends Controller
     List<Station> stations = Station.findAll();
     render ("dashboard.html", stations);
   }
+
+  public static void addStation (String name)
+  {
+    Station station = new Station (name);
+    Logger.info ("Adding a new Station called " + name);
+    station.save();
+    redirect ("/dashboard");
+  }
 }
