8a6ca047b155da562f986855f6ff6a4445f01dcb
Maxi937 <70072337+Maxi937@users.noreply.github.com>
2022-05-27 00:28:08 +0100

    - Release 3: Delete Readings & Delete Stations added

--- app/controllers/StationCtrl.java
+++ app/controllers/StationCtrl.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Member;
 import models.Reading;
 import models.Station;
 import play.Logger;
@@ -26,7 +27,6 @@ public class StationCtrl extends Controller {
     }
 
     public static void addReading(Long id, int code, float temperature, float windSpeed, int windPressure, float windDirection) {
-
            if (code == 0 || temperature == 0 || windSpeed == 0 || windPressure == 0) {
                Logger.info("All fields not completed");
                redirect("/station/" + id);
@@ -39,5 +39,22 @@ public class StationCtrl extends Controller {
            station.save();
            redirect("/station/" + id);
        }
+
+    public static void deleteReading (Long id, Long readingId)
+    {
+        Logger.info("Deleting a Reading: "+ readingId);
+        Station station = Station.findById(id);
+        Reading reading = Reading.findById(readingId);
+        station.readings.remove(reading);
+        station.save();
+        reading.delete();
+
+        Thermometer thermometer = new Thermometer();
+        Anemometer anemometer = new Anemometer();
+        Compass compass = new Compass();
+        render("station.html", station, thermometer, anemometer, compass);
+    }
+
+
 
 }
